f7a3424f84db3b90180e2131ccb6bf9d2d778d71
stejlYura <stejlyura19@gmail.com>
2025-05-12 20:13:03 +0300

    0.0.4

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+    import vueHeader from './components/Header/vue_header.vue'
 </script>
 
 <template>
-
+    <vueHeader></vueHeader>
 </template>
 
 <style scoped lang="sass">
---
+++ src/components/Header/vue_header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>DriveOn</h1>
+        <ul>
+            <li><a href="#">Models</a></li>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    @use '_vue_header.sass';
+</style>
