df94d4380d8fe235a3380a27ed72978dfeabd401
stejlYura <stejlyura19@gmail.com>
2025-05-25 21:31:39 +0300

    Add initial Product type definition and sample products array

---
+++ scripts/createDB.ts
@@ -0,0 +1,12 @@
+type Product = {
+    brand: string;
+    name: string;
+    price: number;
+    description: string;
+}
+
+//{brand: "", name: "", price: 900, description: ""}
+
+const products: Product[] = [
+    {brand: "", name: "", price: 900, description: ""}
+]
