0438df355b21f0679a09bd50a68691d0e5a971c3
YuriiM <stejlyura19@gmail.com>
2025-06-08 10:11:12 +0300

    Update version to 0.2.0 and add reactive products state in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <script setup>
+  import { reactive, provide } from 'vue'
   import VueHeader from '@/components/Header/vueHeader.vue';
   import firstScreen from '@/components/firstScreen/firstScreen.vue';
   import productList from '@/components/product-list/product-list.vue';
+
+    const products = reactive([])
+    provide('products', products)
+
 </script>
 
 <template>
