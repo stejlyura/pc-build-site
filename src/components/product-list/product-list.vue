3f1c7ff16d203e6136392bdfc2809056035e463d
YuriiM <stejlyura19@gmail.com>
2025-06-08 10:23:07 +0300

    Refactor product-list and Home components to use injected products state and improve data handling

--- src/components/product-list/product-list.vue
+++ src/components/product-list/product-list.vue
@@ -21,33 +21,15 @@
 </template>
 
 <script setup lang="ts">
-    import {ref, onMounted} from 'vue'
-    
+  import { inject, ref, type Ref } from 'vue'
   type Product = {
       brand: string;
       name: string;
       type: "GPU" | "CPU" | "RAM" | "SSD" | "HDD" | "PSU" | "CASE" | "COOLER" | "motherboard";
-      socket?: string | "J100"| "ML2";
+      socket?:  "J100" | "ML2" | string;
       price: number;
       description: string;
       image: string; 
   }
-
-const products = ref<Product[]>([])
-const isLoading = ref(true)
-const error = ref<null | string>(null)
-
-onMounted(async () => {
-    try {
-    const response = await fetch('/data/products.json');
-    if (!response.ok) throw new Error('Cant find data');
-    const data = await response.json();
-    products.value = data;
-  } catch (err: any) {
-    error.value = err.message;
-  } finally {
-    isLoading.value = false;
-  }
-})
-//console.log(products)
+  const products = inject<Ref<Product[]>>('products', ref([]))
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,40 @@
-<script setup>
-  import { reactive, provide } from 'vue'
+<script setup lang="ts">
+  import { reactive, provide, ref, onMounted} from 'vue'
   import VueHeader from '@/components/Header/vueHeader.vue';
   import firstScreen from '@/components/firstScreen/firstScreen.vue';
   import productList from '@/components/product-list/product-list.vue';
 
-    const products = reactive([])
+
+
+
+    type Product = {
+      brand: string;
+      name: string;
+      type: "GPU" | "CPU" | "RAM" | "SSD" | "HDD" | "PSU" | "CASE" | "COOLER" | "motherboard";
+      socket?:  "J100" | "ML2" | string;
+      price: number;
+      description: string;
+      image: string; 
+  }
+
+const products = ref<Product[]>([])
+const isLoading = ref(true)
+const error = ref<null | string>(null)
+
+onMounted(async () => {
+    try {
+    const response = await fetch('/data/products.json');
+    if (!response.ok) throw new Error('Cant find data');
+    const data = await response.json();
+    products.value = data;
+  } catch (err: any) {
+    error.value = err.message;
+  } finally {
+    isLoading.value = false;
+  }
+})
+//console.log(products)
+
     provide('products', products)
 
 </script>
