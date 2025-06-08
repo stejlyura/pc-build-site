55225b6b188f34b2648408f5c94ffbf3d727ca0f
YuriiM <stejlyura19@gmail.com>
2025-06-08 09:58:53 +0300

    Add Vue Router integration and create Home view with components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue' 
+import router from './router'
 
 import '@/assets/_main.scss'
 import '@/assets/_normalize.scss'
@@ -8,6 +9,8 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App).mount('#app')
+
+createApp(App).use(router).mount('#app')
 
 App.use(VueSweetalert2);
 App.mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import VueHeader from '@/components/Header/vueHeader.vue';
+  import firstScreen from '@/components/firstScreen/firstScreen.vue';
+  import productList from '@/components/product-list/product-list.vue';
+</script>
+
+<template>
+  <VueHeader />
+  <firstScreen />
+  <productList />
+</template>
+
+<style scoped lang="sass">
+
+</style>
