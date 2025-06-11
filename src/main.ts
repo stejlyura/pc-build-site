f117c66945a621950dcbf48a17f81c36da0983bc
YuriiM <stejlyura19@gmail.com>
2025-06-11 11:46:08 +0300

    Remove SweetAlert2 dependencies and styles; enhance Configurator view with additional grid elements and styles

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,8 @@ import router from './router'
 import '@/assets/_main.scss'
 import '@/assets/_normalize.scss'
 
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
 createApp(App).mount('#app')
 
 createApp(App).use(router).mount('#app')
 
-App.use(VueSweetalert2);
 App.mount('#app');
--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -1,5 +1,24 @@
+<style scoped lang="scss">
+    @use "_configurator.scss";
+</style>
+
 <template>
     <VueHeader />
+    <div class="confugurator-body">
+       <div class="pc">.</div> 
+       <div class="c2">.</div>
+       <div class="c3">.</div>
+       <div class="c4">.</div>
+       <div class="c5">.</div>
+       <div class="c6">.</div>
+       <div class="c7">.</div>
+       <div class="c8">.</div>
+       <div class="c9">.</div>
+       <div class="c10">.</div>
+       <div class="c11">.</div>
+       <div class="c12">.</div>
+       <div class="c13">.</div>
+    </div>
 </template>
 
 <script setup lang="ts">
