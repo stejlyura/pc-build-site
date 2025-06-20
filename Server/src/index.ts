b8c880ae49431f89d8a17747efbece2ebf4b6f7c
YuriiM <stejlyura19@gmail.com>
2025-06-20 09:32:23 +0300

    .............................

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { products } from './data/ppp'
 
 const app = express()
 const PORT = 3000
@@ -8,6 +9,10 @@ async function main() {
 
     app.use('/:username/:cart', (req, res) => {
         res.status(200).json({message: `Hello ${req.params.username}, your cart is empty`})
+    })
+
+    app.use('/products', (req,res) =>{
+        res.status(200).json(products)
     })
 
     app.listen(PORT, () =>{
