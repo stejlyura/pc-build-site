26ccc6ce90dcc0a8dde569c94db16563d2e66b71
YuriiM <stejlyura19@gmail.com>
2025-06-23 14:45:16 +0300

    feat: add mongoose dependency and configure dotenv for environment variables

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,11 +3,15 @@ import { products } from './data/ppp'
 import cors from 'cors';
 import path from 'path'
 import {serviceClient} from './elements/client-site'
+import mongoose from 'mongoose';
+require ('dotenv').config();
 
 const app = express()
 const PORT = 3000
 
 async function main() {
+
+
     app.use(express.json())
 
     app.use(cors())
@@ -15,9 +19,9 @@ async function main() {
     app.use('/public', express.static(path.join(__dirname, '../public')))
 
     app.use('/products', (req,res) =>{
-        res.status(200).json(products) //http://localhost:3000/products
+        res.status(200).json(products)
     })
- 
+
     serviceClient(app)
     
     app.listen(PORT, () =>{
